a0e5ad6301f64d29e226a7a86475fff955b7588c
crm4042 <crm4042@g.rit.edu>
2019-09-10 16:49:31 -0400

    Added file Input

--- JottCompiler/src/src/FileInput.java
+++ JottCompiler/src/src/FileInput.java
@@ -1,6 +1,34 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileInput {
-
+    /**
+     * Reads an entire file and stores the contents in a string
+     *
+     * @param fileName: the name of the file to read
+     *
+     * @return the contents of the file
+     */
+    public static String readFile(String fileName){
+        //The entire file to be returned
+        String file="";
+        try {
+            //The buffered reader to read the file
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(fileName)));
+            //A single line
+            String line="";
+            //Reads the file line-by-line
+            while((line=bufferedReader.readLine())!=null){
+                file+=line;
+            }
+        } catch (IOException e) {
+            System.err.println("Your file was not found");
+            return null;
+        }
+        return file;
+    }
 }
-
