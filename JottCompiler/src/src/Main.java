0c7f748a04787787a3c9737c159b62e6f07c4f89
Paul Avallone <paa9307@rit.edu>
2019-09-10 16:29:08 -0400

    This is Paul's push test

--- JottCompiler/src/src/Main.java
+++ JottCompiler/src/src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code heresfkldsnfj;ldsbfjbdsjlfn
         // Testing Push Nick
+        // Paul Test wailjfa;slijflk;asjf;lkasjdf;lajskdfolksdajfoipjawwe;ifjaj;silfj;aslkfdj;i$
     }
 }
