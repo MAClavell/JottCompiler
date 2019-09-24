e2569b1824e9f25c4987e1b8e8159bab5c286ad5
Paul Avallone <paa9307@rit.edu>
2019-09-24 16:09:29 -0400

    Empty Predict functions in Parser

--- Jott/src/Parser.java
+++ Jott/src/Parser.java
@@ -10,4 +10,73 @@ public class Parser {
         TreeNode root=new TreeNode(null);
         return root;
     }
+
+    ///Predict Functions
+    public void program(TreeNode node){
+
+    }
+    public void stmt_list(TreeNode node){
+
+    }
+    public void end_paren(TreeNode node) {
+
+    }
+    public void end_stmt(TreeNode node) {
+
+    }
+    public void character(TreeNode node) {
+
+    }
+    public void l_char(TreeNode node) {
+
+    }
+    public void u_char(TreeNode node) {
+
+    }
+    public void digit(TreeNode node) {
+
+    }
+    public void sign(TreeNode node) {
+
+    }
+    public void id(TreeNode node) {
+
+    }
+    public void stmt(TreeNode node) {
+
+    }
+    public void expr(TreeNode node) {
+
+    }
+    public void print(TreeNode node) {
+
+    }
+    public void asmt(TreeNode node) {
+
+    }
+    public void op(TreeNode node) {
+
+    }
+    public void dbl(TreeNode node) {
+
+    }
+    public void d_expr(TreeNode node) {
+
+    }
+    public void integer(TreeNode node) {
+
+    }
+    public void i_expr(TreeNode node) {
+
+    }
+    public void str_literal(TreeNode node) {
+
+    }
+    public void str(TreeNode node) {
+
+    }
+    public void s_expr(TreeNode node) {
+
+    }
+
 }
