80b200a6cded893ab0f1a18d0d54dedda951a398
crm4042 <crm4042@g.rit.edu>
2019-09-24 13:09:28 -0400

    Added some functionality to the TreeNode class

--- Jott/src/TreeNode.java
+++ Jott/src/TreeNode.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class TreeNode {
+
+    private ArrayList<TreeNode> branches;
+    private String contents;
+
+    public TreeNode(String contents){
+        branches=new ArrayList<TreeNode>();
+        this.contents=contents;
+    }
+
+    public boolean hasChildren(){
+        return branches.size()!=0;
+    }
+
+    public void addTreeNode(String contents){
+        branches.add(new TreeNode(contents));
+    }
+
+    public String getContents(){
+        return contents;
+    }
+
+    @Override
+    public String toString() {
+        String builtString=contents;
+        for(TreeNode childBranches:branches){
+            contents+=childBranches.branches;
+        }
+        return builtString;
+    }
 }
