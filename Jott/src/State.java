348da7a61f585b9c83a2b2ece5695cd0e9e124fb
crm4042 <crm4042@g.rit.edu>
2019-09-24 14:49:12 -0400

    Added the enum to the state class. This is a requirement for the parse tree.

--- Jott/src/State.java
+++ Jott/src/State.java
@@ -1,6 +1,8 @@
 public class State {
-    public enum stateType{
-
+    //The type of state that the node represents
+    enum stateType{
+        PROGRAM, STMT_LIST, START_PAREN, END_PAREN, END_STATEMENT, CHAR, L_CHAR, U_CHAR, DIGIT,
+        SIGN, ID, STMT, EXPR, PRINT, ASMT, OP, DBL, D_EXPR, INT, I_EXPR, STR_LITERAL, STR, S_EXPR;
     }
 
     //The State that the parse tree is in
