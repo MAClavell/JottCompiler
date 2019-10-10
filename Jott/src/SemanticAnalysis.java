2bc6af3608436105602f09af0f531af1b927bf9a
crm4042 <crm4042@g.rit.edu>
2019-10-10 13:56:00 -0400

    Finished the i_expr

--- Jott/src/SemanticAnalysis.java
+++ Jott/src/SemanticAnalysis.java
@@ -95,119 +95,93 @@ public class SemanticAnalysis {
         return false;
     }
 
-    private static int i_expr(TreeNode node){
+    private static int parseIntOperand(TreeNode node, int childNumber){
+        TreeNode childNode=node.getChildren().get(childNumber);
 
-        // If the leftmost child is an id
-        if(node.getChildren().get(0).getState().getState() == State.stateType.ID){
-
-            TreeNode childID=node.getChildren().get(0);
-
-            // It is in the symbol table as the same type and is not undefined
-            if(verifyIntID(childID)){
-
-                // The first operator
-                Integer firstOp=(Integer) symbols.get(childID.getToken().getTokenText()).getValue();;
-
-                // If there is a second child
-                if(node.getChildren().size()>1){
-
-                    Integer secondOp;
-
-                    // If the second operation is an id
-                    if(node.getChildren().get(2).getState().getState() == State.stateType.ID){
-                        if(verifyIntID(node.getChildren().get(2))){
-                            secondOp=(Integer)symbols.get(node.getChildren().get(2).getToken().getTokenText()).getValue();
-                        }
-                        else {
-                            // TODO ERROR
-                        }
-                    }
-
-                    // If the second operation is an Integer (or +/-)
-                    else if(node.getChildren().get(2).getState().getState()== State.stateType.INT ||
-                            node.getChildren().get(2).getState().getState()== State.stateType.SIGN){
-
-                    }
-
-                    // If the second operation is an i_expr
-                    else{
-
-                    }
-
-                    // Return statement based on the operation (+,-,/,*)
-                    switch(node.getChildren().get(1).getToken().getTokenType()){
-                        case Minus:
-                            return firstOp-secondOp;
-                        case Plus:
-                            return firstOp+secondOp;
-                        case Mult:
-                            return firstOp*secondOp;
-                        case Divide:
-                            return firstOp/secondOp;
-                    }
-                }
-
-                // If this is the only state in the expression return it (autoboxes)
-                else{
-                    return firstOp;
-                }
+        // If it is an id
+        if(childNode.getState().getState()== State.stateType.ID){
+            // Verifies it is defined, not null, and it is an integer
+            if(verifyIntID(node)){
+                return (Integer)symbols.get(childNode.getToken().getTokenText()).getValue();
             }
-
-            //TODO ERROR
+            // Error
         }
 
-        // If the leftmost child is an Integer
-        else if(node.getChildren().get(0).getState().getState()== State.stateType.INT ||
-                node.getChildren().get(0).getState().getState()== State.stateType.SIGN){
+        // If it is an integer
+        else if(childNode.getState().getState()== State.stateType.INT ||
+                childNode.getState().getState()== State.stateType.SIGN){
+            int secondOp;
 
-            /*
-             *   Gets the first integer
-             */
+            // If the first symbol is a sign
+            if(childNode.getState().getState()== State.stateType.SIGN){
+                secondOp=Integer.parseInt(node.getChildren().get(childNumber+1).getToken().getTokenText());
 
-            Integer firstOp;
-
-            // There is no sign
-            if(node.getChildren().size()==1){
-                firstOp=Integer.parseInt(node.getChildren().get(0).getToken().getTokenText());
+                if(childNode.getToken().getTokenType()==TokenType.Minus){
+                    secondOp*=-1;
+                }
+                return secondOp;
             }
 
-            // There is a sign
+            // If there is no sign
             else{
-                firstOp=Integer.parseInt(node.getChildren().get(1).getToken().getTokenText());
-
-                // Adds a negative sign
-                if(node.getChildren().get(0).getToken().getTokenType()==TokenType.Minus){
-                    firstOp*=-1;
-                }
+                return Integer.parseInt(childNode.getToken().getTokenText());
             }
-
-            /*
-             * Parses a second operand if there is one and handles returning
-             */
-            if(node.getChildren().size() > 2){
-                if(){
-
-                }
-
-                else if(){
-
-                }
-                else{
-
-                }
-            }
-
-            // There is only one op
-            else{
-                return firstOp;
-            }
-
         }
 
-        // If the leftmost child is an i_expr
+        // If it is another i_expr
         else{
-
+            return i_expr(childNode);
         }
+
+        // This should never be reached
+        return -1;
+    }
+
+    private static int i_expr(TreeNode node){
+        // Gets the first operator
+        int firstOp=parseIntOperand(node, 0);
+
+        // There are multiple operators
+        if(node.getChildren().size()>2) {
+
+            int secondOp;
+            int sign;
+            // The first operator had a sign
+            if (node.getChildren().get(0).getState().getState() == State.stateType.SIGN) {
+                sign=2;
+
+            }
+
+            // The first operator did not have a sign
+            else {
+                sign=1;
+            }
+
+            // Gets the second operand
+            secondOp=parseIntOperand(node, sign+1);
+
+            // Uses the sign number
+            switch(node.getChildren().get(sign).getToken().getTokenType()){
+                case Minus:
+                    return firstOp-secondOp;
+                case Plus:
+                    return firstOp+secondOp;
+                case Mult:
+                    return firstOp*secondOp;
+                case Divide:
+                    return firstOp/secondOp;
+            }
+
+            //TODO error
+        }
+
+        // It is just one operator
+        else{
+            return firstOp;
+        }
+
+        // This should never be reached
+        return -1;
     }
 
     private static double d_expr(TreeNode node){
